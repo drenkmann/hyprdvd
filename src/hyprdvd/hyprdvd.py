6a3157d17eeb7eb26c3890f90a0ad2b68aa48868
Nevimmu <nevimmu@gmail.com>
2025-10-13 18:18:51 +0200

    feat: added collision logic between the DVD term

--- src/hyprdvd/hyprdvd.py
+++ src/hyprdvd/hyprdvd.py
@@ -1,13 +1,13 @@
 from socket import socket, AF_UNIX, SOCK_STREAM
-from multiprocessing import Process
 import json
 import random
 import math
+import time
 from .settings import SOCKET_PATH
-from .utils	import hyprctl
+from .utils import hyprctl
 
-class hyprdvd():
-	'''Main class for hyprdvd.'''
+class HyprDVD:
+	"""Class for a single bouncing window."""
 	def __init__(self, event_data):
 		self.address = f'0x{event_data[0]}'
 		self.workspace_id = int(event_data[1])
@@ -21,111 +21,170 @@ class hyprdvd():
 		self.velocity_y = 2
 
 		self.set_window_start()
-		self.loop()
-
-		if self.get_animation_option:
-			hyprctl(['keyword', 'animations:enabled', 'yes'])
-		else:
-			hyprctl(['keyword', 'animations:enabled', 'no'])
-
-	def loop(self):
-		'''Main loop'''
-		while True:
-			if not self.get_window_position_and_size():
-				break
-			self.handle_animation()
-
-			if self.window_y + self.window_height + self.border_size + self.velocity_y > self.screen_height or \
-			self.window_y + self.velocity_y < 0:
-				self.velocity_y *= -1
-
-			if self.window_x + self.window_width + self.border_size + self.velocity_x > self.screen_width or \
-			self.window_x + self.velocity_y < 0:
-				self.velocity_x *= -1
-
-			hyprctl(['dispatch', 'movewindowpixel', 'exact', 
-							str(self.window_x + self.velocity_x) , str(self.window_y + self.velocity_y), f',address:{self.address}'
-			])
 
 	def set_window_size(self):
-		'''Set the size of the window relative to the screen size'''
+		"""Set the size of the window relative to the screen size"""
 		resize = 0.4
-
 		self.window_width = math.ceil(self.screen_width * resize)
 		self.window_height = math.ceil(self.screen_height * resize)
 
 	def set_window_start(self):
-		'''Set a random positon and direction'''
-		random_x = random.randrange(0, self.screen_width - self.window_width)
-		random_y = random.randrange(0, self.screen_height - self.window_height)
-
+		"""Set a random direction"""
 		if random.randrange(1, 100) % 2 == 0:
 			self.velocity_x *= -1
-
 		if random.randrange(101, 200) % 2 == 0:
 			self.velocity_y *= -1
 
 		hyprctl(['dispatch', 'setfloating', f'address:{self.address}'])
-		hyprctl(['dispatch', 'resizewindowpixel', 'exact', 
-						str(self.window_width), str(self.window_height), f',address:{self.address}'
-		])
-		hyprctl(['dispatch', 'movewindowpixel', 'exact', 
-						str(random_x) , str(random_y), f',address:{self.address}'
-		])
+		hyprctl(['dispatch', 'resizewindowpixel', 'exact',
+				 str(self.window_width), str(self.window_height), f',address:{self.address}'])
 
 	def get_screen_size(self):
-		'''Get the screen size'''
+		"""Get the screen size"""
 		monitors_json = json.loads(hyprctl(['monitors', '-j']).stdout)
 		for monitor in monitors_json:
 			if monitor['activeWorkspace']['id'] == int(self.workspace_id):
 				transform = monitor['transform'] in [1, 3, 5, 7]
 				self.screen_width = monitor['width'] if not transform else monitor['height']
 				self.screen_height = monitor['height'] if not transform else monitor['width']
-				# TODO: set correct screen size for side monitors
-				# self.screen_x = monitor['x']
-				# self.screen_y = monitor['y']
 				break
 
 	def get_window_position_and_size(self):
-		'''Get the window position'''
+		"""Get the window position and size"""
 		clients = json.loads(hyprctl(['clients', '-j']).stdout)
-		workspace_windows = [c for c in clients if c['workspace']['id'] == self.workspace_id]
-		window = next((w for w in workspace_windows if w['address'] == self.address), None)
+		window = next((w for w in clients if w['address'] == self.address), None)
 
 		if not window:
 			return False
 
 		self.window_x, self.window_y = window['at']
-		self.window_width, self.window_height = window['size'] # If window get resized
+		self.window_width, self.window_height = window['size']
 		return True
 
-	def get_animation_option(self):
-		'''Get the animation option value'''
-		self.animation_option = hyprctl(['getoption', 'animations:enabled']).stdout.split()[1]
+	def update(self):
+		"""Update window position"""
+		self.window_x += self.velocity_x
+		self.window_y += self.velocity_y
 
-	def handle_animation(self):
-		'''Handle the animation'''
-		if self.get_active_workspace() == self.workspace_id:
+	def move(self):
+		"""Move window to its new position"""
+		hyprctl(['dispatch', 'movewindowpixel', 'exact',
+				 str(self.window_x), str(self.window_y), f',address:{self.address}'])
+
+class HyprDVDManager:
+	"""Manages all HyprDVD windows."""
+	def __init__(self):
+		self.windows = []
+		self.animation_enabled_workspaces = {}
+
+	def add_window(self, event_data):
+		"""Add a new window to manage"""
+		window = HyprDVD(event_data)
+
+		attempts = 0
+		while attempts < 100:
+			random_x = random.randrange(0, window.screen_width - window.window_width)
+			random_y = random.randrange(0, window.screen_height - window.window_height)
+
+			overlapping = False
+			for other_window in self.windows:
+				if (window.workspace_id == other_window.workspace_id and
+						random_x < other_window.window_x + other_window.window_width and
+						random_x + window.window_width > other_window.window_x and
+						random_y < other_window.window_y + other_window.window_height and
+						random_y + window.window_height > other_window.window_y):
+					overlapping = True
+					break
+			if not overlapping:
+				hyprctl(['dispatch', 'movewindowpixel', 'exact',
+						 str(random_x), str(random_y), f',address:{window.address}'])
+				self.windows.append(window)
+				self.handle_animation(window.workspace_id, True)
+				return
+
+			attempts += 1
+
+		# If no space is found after 100 attempts, close the window
+		hyprctl(['dispatch', 'closewindow', f'address:{window.address}'])
+
+	def check_collisions(self):
+		"""Check for collisions between windows and with screen borders."""
+		for i, window in enumerate(self.windows):
+			# Screen border collision
+			if not 0 < window.window_x < window.screen_width - window.window_width:
+				window.velocity_x *= -1
+			if not 0 < window.window_y < window.screen_height - window.window_height:
+				window.velocity_y *= -1
+
+			# Other window collision
+			for other_window in self.windows[i+1:]:
+				if (
+					window.workspace_id == other_window.workspace_id and
+					window.window_x < other_window.window_x + other_window.window_width and
+					window.window_x + window.window_width > other_window.window_x and
+					window.window_y < other_window.window_y + other_window.window_height and
+					window.window_y + window.window_height > other_window.window_y
+				):
+
+					overlap_x = min(window.window_x + window.window_width, other_window.window_x + other_window.window_width) - max(window.window_x, other_window.window_x)
+					overlap_y = min(window.window_y + window.window_height, other_window.window_y + other_window.window_height) - max(window.window_y, other_window.window_y)
+
+					if overlap_x < overlap_y:
+						window.velocity_x, other_window.velocity_x = other_window.velocity_x, window.velocity_x
+					else:
+						window.velocity_y, other_window.velocity_y = other_window.velocity_y, window.velocity_y
+
+
+
+	def update_windows(self):
+		"""Update all window positions and move them."""
+		for window in self.windows:
+			if not window.get_window_position_and_size():
+				self.windows.remove(window)
+				self.handle_animation(window.workspace_id, False)
+				continue
+			window.update()
+
+		self.check_collisions()
+
+		for window in self.windows:
+			window.move()
+
+	def handle_animation(self, workspace_id, is_enabled):
+		"""Handle animations for the workspace."""
+		if is_enabled:
+			if workspace_id not in self.animation_enabled_workspaces:
+				self.animation_enabled_workspaces[workspace_id] = hyprctl(['getoption', 'animations:enabled']).stdout.split()[1]
 			hyprctl(['keyword', 'animations:enabled', 'no'])
 		else:
-			hyprctl(['keyword', 'animations:enabled', 'yes'])
-
-	def get_active_workspace(self):
-		'''Get the active workspace'''
-		return json.loads(hyprctl(['activeworkspace', '-j']).stdout)['id']
-
+			if workspace_id in self.animation_enabled_workspaces:
+				hyprctl(['keyword', 'animations:enabled', self.animation_enabled_workspaces.pop(workspace_id)])
 
 def main():
-	'''Main function of the script.'''
+	"""Main function of the script."""
+	manager = HyprDVDManager()
 
 	# Connect to Hyprland's socket and listen for events.
 	with socket(AF_UNIX, SOCK_STREAM) as sock:
 		sock.connect(SOCKET_PATH)
+		sock.setblocking(False)
+
 		while True:
-			event = sock.recv(1024).decode().strip().split('\n')[0]
-			if event:
-				event_type, event_data = event.split('>>', 1)
-				event_data = event_data.split(',')
-				if event_type == 'openwindow' and event_data[3] == 'DVD':
-					process = Process(target=hyprdvd, args=(event_data,))
-					process.start()
+			try:
+				event = sock.recv(4096).decode().strip()
+				if event:
+					for line in event.split('\n'):
+						event_type, event_data = line.split('>>', 1)
+						event_data = event_data.split(',')
+						if event_type == 'openwindow' and len(event_data) > 3 and event_data[3] == 'DVD':
+							manager.add_window(event_data)
+			except BlockingIOError:
+				pass
+
+			if manager.windows:
+				manager.update_windows()
+
+			time.sleep(0.01) # Control the loop speed
+
+if __name__ == "__main__":
+	main()
